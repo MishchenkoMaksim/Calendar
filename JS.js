e46684e1da3d3d0a485bc7155c1addf6e07e6c5f
Maksim Mishchenko <max75583@gmail.com>
2019-06-17 12:33:32 +0300

    Добавлен функционал

--- JS.js
+++ JS.js
@@ -1,9 +1,15 @@
 ﻿window.onload = function () {
 
-    var all_td = 0;
-    var now = new Date();
-    var month = new Date().getMonth();
-    var today = now.getDate();
+    var shwDate = document.getElementById('date'),
+        all_td = 0,
+        now = new Date(),
+        month = new Date().getMonth(),
+        today = now.getDate(),
+        nxt = document.getElementById('nxt'),
+        prev = document.getElementById('prev'),
+        monthNames = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        ];
 
     function createCalend(num) {
         month += (num || 0);
@@ -37,7 +43,16 @@
 
         text += '</tr></table>';
         document.getElementById('calendar').innerHTML = text;
-        console.log(now);
+        shwDate.innerHTML = monthNames[now.getMonth()];
+
     }
     createCalend();
+
+    nxt.onclick = function () {
+        createCalend(1);
+    }
+
+    prev.onclick = function () {
+        createCalend(-1);
+    }
 }
