61e5575ba52705f60063cd4735ab6e163a9ebad1
Maksim Mishchenko <max75583@gmail.com>
2019-06-18 13:11:35 +0300

    Добавлен style.css
    Добавлены обработчики событий, дни недели, добавлены стили

--- JS.js
+++ JS.js
@@ -7,9 +7,16 @@
         today = now.getDate(),
         nxt = document.getElementById('nxt'),
         prev = document.getElementById('prev'),
+
         monthNames = ["January", "February", "March", "April", "May", "June",
             "July", "August", "September", "October", "November", "December"
-        ];
+        ],
+
+        row = document.getElementsByTagName('td'),
+        modal = document.getElementsByClassName('modal')[0],
+        save = document.getElementById('save'),
+        cncl = document.getElementById('cncl'),
+        txt = document.getElementById('txt');
 
     function createCalend(num) {
         month += (num || 0);
@@ -18,6 +25,10 @@
         now.setDate(1);
         var blank = (now.getDay() == 0) ? 6 : now.getDay() - 1;
         var text = '<table><tr>';
+
+
+
+        text += '<div class="days"><span>Monday</span><span>Tuesday</span><span>Wednesday</span><span>Thursday</span><span>Friday</span><span>Saturday</span><span>Sunday</span></div>';
 
         for (var i = 0; i < blank; i++) {
             text += '<td>&nbsp;</td>';
@@ -30,6 +41,7 @@
 
         for (var k = 1; k <= days; k++) {
             text += '<td>' + k + '</td>';
+            console.log(k);
             all_td++;
             if (all_td % 7 == 0) { text += '</tr><tr>'; }
         }
@@ -45,6 +57,29 @@
         document.getElementById('calendar').innerHTML = text;
         shwDate.innerHTML = monthNames[now.getMonth()];
 
+        for (var i = 0; i < row.length; i++) {
+
+            var t;
+
+            row[i].style.width = "10%";
+
+            row[i].addEventListener('click', function () {
+                t = this;
+                modal.classList.toggle('none');
+            });
+
+            save.onclick = function () {
+                var p = document.createElement('p');
+                p.style.fontSize = "15px";
+                p.style.textOverflow = 'ellipsis';
+                p.style.backgroundColor = 'mediumaquamarine';
+                p.innerHTML = txt.value;
+                t.appendChild(p);
+                modal.classList.toggle('none');
+                txt.value = '';
+            }
+        }
+
     }
     createCalend();
 
@@ -54,5 +89,10 @@
 
     prev.onclick = function () {
         createCalend(-1);
+    }
+
+    cncl.onclick = function () {
+        modal.classList.toggle('none');
+        txt.value = '';
     }
 }
